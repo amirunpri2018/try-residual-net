f86f0bb1657484f8f90f8e3522dbc05aaddfb760
OJIMA Ryoji <rojima1@gmail.com>
2017-10-25 11:49:47 +0900

    tuned.

--- train.py
+++ train.py
@@ -1,3 +1,12 @@
+# Implementation of ResNet-164
+#
+# Deep Residual Learning for Image Recognition
+# https://arxiv.org/abs/1512.03385
+#
+# Identity Mappings in Deep Residual Networks
+# https://arxiv.org/abs/1603.05027)# Wide Residual Network
+
+
 import numpy as np
 import pickle
 
@@ -8,65 +17,87 @@ from keras.layers              import Activation, Add, BatchNormalization, Conv2
 from keras.models              import Model, save_model
 from keras.optimizers          import SGD
 from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers        import l2
+from keras.utils               import plot_model
 from operator                  import getitem
 
 
-def ljuxt(*fs):  # Kerasはジェネレーターを引数に取るのを嫌がるみたい、かつ、funcyはPython3だと積極的にジェネレーターを使うみたいなので、リストを返すjuxtを作りました。
-    return rcompose(juxt(*fs), list)
+def computational_graph(class_size):
+    # Utility functions.
 
+    def ljuxt(*fs):  # Kerasはジェネレーターを引数に取るのを嫌がるみたい、かつ、funcyはPython3だと積極的にジェネレーターを使うみたいなので、リストを返すjuxtを作りました。
+        return rcompose(juxt(*fs), list)
 
-def create_model(x, y):
-    def conv_2d(filters, kernel_size, strides=1):
-        return Conv2D(filters, kernel_size, strides=strides, padding='same', kernel_initializer='he_normal', use_bias=False)
+    def batch_normalization():
+        return BatchNormalization()
 
-    def bn_relu():
-        return rcompose(BatchNormalization(), Activation('relu'))
+    def relu():
+        return Activation('relu')
 
-    def bn_relu_conv(filters, kernel_size, strides=1):
-        return rcompose(bn_relu(), conv_2d(filters, kernel_size, strides))
+    def conv(filter_size, kernel_size, stride_size=1):
+        return Conv2D(filter_size, kernel_size, strides=stride_size, padding='same', kernel_initializer='he_normal', kernel_regularizer=l2(0.0001), use_bias=False)
 
-    def first_residual_unit(filters, strides):
-        return rcompose(bn_relu(),
-                        ljuxt(rcompose(conv_2d(filters // 4, 1, strides),
-                                       bn_relu(),
-                                       conv_2d(filters // 4, 3),
-                                       bn_relu(),
-                                       conv_2d(filters,      1)),
-                              rcompose(conv_2d(filters,      1, strides))),
-                        Add())
+    def add():
+        return Add()
 
-    def residual_unit(filters):
-        return rcompose(ljuxt(rcompose(bn_relu_conv(filters // 4, 1),
-                                       bn_relu_conv(filters // 4, 3),
-                                       bn_relu_conv(filters,      1)),
+    def global_average_pooling():
+        return GlobalAveragePooling2D()
+
+    def dense(unit_size, activation):
+        return Dense(unit_size, activation=activation, kernel_regularizer=l2(0.0001))
+
+    # Define ResNet-164
+
+    def first_residual_unit(filter_size, stride_size):
+        return rcompose(batch_normalization(),
+                        relu(),
+                        ljuxt(rcompose(conv(filter_size // 4, 1, stride_size),
+                                       batch_normalization(),
+                                       relu(),
+                                       conv(filter_size // 4, 3),
+                                       batch_normalization(),
+                                       relu(),
+                                       conv(filter_size, 1)),
+                              rcompose(conv(filter_size, 1, stride_size))),
+                        add())
+
+    def residual_unit(filter_size):
+        return rcompose(ljuxt(rcompose(batch_normalization(),
+                                       relu(),
+                                       conv(filter_size // 4, 1),
+                                       batch_normalization(),
+                                       relu(),
+                                       conv(filter_size // 4, 3),
+                                       batch_normalization(),
+                                       relu(),
+                                       conv(filter_size, 1)),
                               identity),
-                        Add())
+                        add())
 
-    def residual_block(filters, strides):
-        return rcompose(first_residual_unit(filters, strides),
-                        rcompose(*repeatedly(partial(residual_unit, filters), 18 - 1)))
+    def residual_block(filter_size, stride_size, unit_size):
+        return rcompose(first_residual_unit(filter_size, stride_size),
+                        rcompose(*repeatedly(partial(residual_unit, filter_size), unit_size - 1)))
 
-    computational_graph = rcompose(conv_2d(16, 3),
-                                   residual_block( 64, 1),
-                                   residual_block(128, 2),
-                                   residual_block(256, 2),
-                                   BatchNormalization(),
-                                   Activation('relu'),
-                                   GlobalAveragePooling2D(),
-                                   Dense(y.shape[1], activation='softmax'))
+    n = 18  # ResNet-164の164は、1（最初） + 3 * n * 3 + 1（最後）みたい。
 
-    return Model(*juxt(identity, computational_graph)(Input(shape=x.shape[1:])))
+    return rcompose(conv(16, 3),
+                    residual_block( 64, 1, n),
+                    residual_block(128, 2, n),
+                    residual_block(256, 2, n),
+                    batch_normalization(),
+                    relu(),
+                    global_average_pooling(),
+                    dense(class_size, 'softmax'))
 
 
 def main():
     (x_train, y_train), (x_validation, y_validation) = load_data()
 
-    model = create_model(x_train, y_train)
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=SGD(momentum=0.9, decay=0.0001, nesterov=True),
-                  metrics=['accuracy'])
+    model = Model(*juxt(identity, computational_graph(y_train.shape[1]))(Input(shape=x_train.shape[1:])))
+    model.compile(loss='categorical_crossentropy', optimizer=SGD(momentum=0.9), metrics=['accuracy'])
 
     model.summary()
+    plot_model(model, to_file='./results/model.png')
 
     train_data      = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True, width_shift_range=0.125, height_shift_range=0.125, horizontal_flip=True)
     validation_data = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True)
@@ -74,21 +105,20 @@ def main():
     for data in (train_data, validation_data):
         data.fit(x_train)  # 実用を考えると、x_validationでのfeaturewiseのfitは無理だと思う……。
 
-    batch_size = 64
-    epochs     = 200
+    batch_size = 128
+    epoch_size = 200
 
     results = model.fit_generator(train_data.flow(x_train, y_train, batch_size=batch_size),
                                   steps_per_epoch=x_train.shape[0] // batch_size,
-                                  epochs=epochs,
-                                  callbacks=[LearningRateScheduler(partial(getitem, tuple(take(epochs, concat(repeat(0.01, 1), repeat(0.1, 79), repeat(0.01, 42), repeat(0.001))))))],
+                                  epochs=epoch_size,
+                                  callbacks=[LearningRateScheduler(partial(getitem, tuple(take(epoch_size, concat(repeat(0.1, 80), repeat(0.01, 42), repeat(0.001))))))],
                                   validation_data=validation_data.flow(x_validation, y_validation, batch_size=batch_size),
-                                  validation_steps=x_validation.shape[0] // batch_size,
-                                  workers=4)
+                                  validation_steps=x_validation.shape[0] // batch_size)
 
-    with open('./history.pickle', 'wb') as f:
+    with open('./results/history.pickle', 'wb') as f:
         pickle.dump(results.history, f)
 
-    save_model(model, './model.h5')
+    save_model(model, './results/model.h5')
 
     del model
 
